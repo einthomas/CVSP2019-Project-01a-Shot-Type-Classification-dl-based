0f796fd4e1ad3f1ddbb62907461b2cfde71707b7
einthomas <thomas-koch@kabelplus.at>
2019-12-03 16:26:27 +0100

    Implement test data loading

--- trainNetwork.py
+++ trainNetwork.py
@@ -1,6 +1,14 @@
-# tensorflow and keras
 import tensorflow as tf
 from tensorflow import keras
+from keras.preprocessing import image
+from keras.utils import to_categorical
+import os
+import csv
+import numpy as np
+import pandas as pd
+
+
+######### MODEL LOADING #########
 
 # Load pretrained model for transfer learning
 oldModel = keras.models.load_model('D:\\CSVP2019\\model\\model_shotscale_967.h5')
@@ -25,3 +33,39 @@ checkpoint = keras.callbacks.ModelCheckpoint(
     filepath='D:\\CSVP2019\\model\\model_transfer.h5',
     save_best_only=True
 )
+
+
+######### LOAD DATA #########
+
+shotTypes = ['CU', 'MS', 'LS', 'ELS']
+targetSize = (125, 224, 3)
+
+# Load frames and labels from CSV
+def parseAnnotationsCSV(csvPath, framesPath, targetSize):
+    frames = []
+    labels = []
+    
+    csvReader = csv.reader(open(csvPath), delimiter=';')
+    for row in csvReader:
+        # Skip header
+        if csvReader.line_num == 1:
+            continue
+
+        frameName = row[1]
+        frameNumber = row[2]
+        label = row[3]
+        
+        if label != 'NONE':
+            labels.append(shotTypes.index(label))
+
+            # Load and scale image
+            img = image.load_img(os.path.join(framesPath, frameName + '.jpg'), target_size=targetSize)
+            img = image.img_to_array(img)
+            img = img / 255.0
+            frames.append(img)
+    
+    return np.array(frames), np.array(labels)
+
+trainFramesPath = 'D:\\CSVP2019\\copied\\data\\test\\imc'
+trainAnnotationsCSVPath = 'D:\\CSVP2019\\copied\\annotations\\annotations_test_imc.csv'
+trainFrames, trainLabels = parseAnnotationsCSV(trainAnnotationsCSVPath, trainFramesPath, targetSize)
